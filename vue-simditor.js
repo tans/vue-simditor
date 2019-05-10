3f1cdbe202599d087333f82c9caf8ee57ac4a1e3
tans <369600584@qq.com>
2019-05-10 11:29:42 +0800

    -

--- vue-simditor.js
+++ vue-simditor.js
@@ -1,20 +1,32 @@
 Vue.component('simditor', {
-    props: ['value', 'id' ,  'toolbar'],
-    template: `
-      <textarea
-        v-bind:id="'simditor'+id"
-        v-bind:value="value"
-        v-on:input="onInput"
-      ></textarea>
-    `,
-    mounted:function(){
-        var self = this;
-        this.editor = new Simditor({
-            toolbar: this.toolbar|| [],
-            textarea: $('#simditor'+this.id)
-        });
-        this.editor.on('valuechanged', function(){
-            self.value = self.editor.getValue();
-        });
+  props: ['value', 'id' , 'toolbar'],
+  template: `
+    <textarea
+      v-bind:id="'simditor'+id"
+      v-bind:value="value"
+    ></textarea>
+  `,
+  data: function(){
+    return {
+      edited: false
     }
-  })
+  },
+  mounted:function(){
+      var self = this;
+      this.editor = new Simditor({
+          toolbar: this.toolbar|| [],
+          textarea: $('#simditor'+this.id)
+      });
+      this.editor.on('valuechanged', function(){
+        self.edited = true;
+        self.$emit('input', self.editor.getValue());
+      });
+  },
+  watch:{
+    value:function(a){
+      if(!this.edited){
+        this.editor.setValue(a);
+      }
+    }
+  }
+})
